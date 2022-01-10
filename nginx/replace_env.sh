23207f8b03a00b23201a0dab54e9a7ee817ce148
Juntao Wu <wujuntaocn@outlook.com>
2022-01-10 09:51:32 +0800

    brand_name env

--- nginx/replace_env.sh
+++ nginx/replace_env.sh
@@ -2,6 +2,7 @@
 if [ "$REPLACE_ENV" = "REPLACE" ]; \
 then echo REPLACE_ENV:REPLACE \
 && sed -i 's@base href="/"@base href="'"$BASE_VIRTUAL_PATH"'@g' /usr/share/nginx/html/index.html \
+&& sed -i 's@BRAND_NAME@'"$BRAND_NAME"'@g' /usr/share/nginx/html/index.html \
 && find /usr/share/nginx/html -type f -name "*.js" | xargs sed -i 's@API_ENV@'"$API_ENV"'@g' \
 && find /usr/share/nginx/html -type f -name "*.js" | xargs sed -i 's@BRAND_NAME@'"$BRAND_NAME"'@g'; \
 else \
